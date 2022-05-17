c8f8d9cf7f3c940c912ba3d587d065ab31fd6a1e
Vikas <vikassharmanaini@gmail.com>
2022-05-17 13:34:59 +0530

    Second day work Completed

--- src/com/Suven/Consultants/app.java
+++ src/com/Suven/Consultants/app.java
@@ -2,12 +2,24 @@ package com.Suven.Consultants;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class app{
+    private boolean method=true;
     private JPanel label1;
     private JTextField textField1;
+
+    public boolean isMethod() {
+        return method;
+    }
+
+    public void setMethod(boolean method) {
+        this.method = method;
+    }
+
     private JTextField textField2;
     private JTextField textField3;
     private JButton xButton;
@@ -26,6 +38,11 @@ public class app{
         xButton1.setVisible(false);
         textField4.setBackground(Color.YELLOW);
         textField4.setEnabled(false);
+        newLoanAnalysisButton.setEnabled(false);
+        textArea1.setFont(new Font("Courier New", Font.PLAIN, 14));
+        textArea1.setEditable(false);
+        textArea1.setPreferredSize(new Dimension(250, 150));
+        textArea1.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         xButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -36,8 +53,12 @@ public class app{
                 xButton1.setVisible(true);
                 textField4.setBackground(Color.WHITE);
                 textField4.setEnabled(true);
+                setMethod(true);
+                System.out.println(method);
             }
         });
+
+
         xButton1.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -48,13 +69,42 @@ public class app{
                 xButton.setVisible(true);
                 textField3.setBackground(Color.WHITE);
                 textField3.setEnabled(true);
+                setMethod(false);
+                System.out.println(method);
+            }
+        });
+        computeMonthlyPaymentsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (method){
+
+                   textField4.setText(String.valueOf(new LoanCalculator().paid(Double.parseDouble(textField2.getText()),Double.parseDouble(textField1.getText()),Integer.parseInt(textField3.getText()))));
+                }
+                else {
+                    textField3.setText(String.valueOf(new LoanCalculator().findhowmuchtimes(Double.parseDouble(textField2.getText()),Double.parseDouble(textField1.getText()),Double.parseDouble(textField4.getText()))));
+                }
+                newLoanAnalysisButton.setEnabled(true);
+                computeMonthlyPaymentsButton.setEnabled(false);
+            }
+        });
+        newLoanAnalysisButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                newLoanAnalysisButton.setEnabled(false);
+                computeMonthlyPaymentsButton.setEnabled(true);
+                textField1.setText("");
+                textField2.setText("");
+                textField4.setText("");
+                textField3.setText("");
             }
         });
     }
 
     public void lunch(){
         JFrame f=new JFrame();
-        xButton1.setVisible(false);
+        f.setLocation(400,200);
+        f.setFocusable(true);
+        f.setResizable(false);
         f.setContentPane(new app().label1);
         f.pack();
         f.setVisible(true);
