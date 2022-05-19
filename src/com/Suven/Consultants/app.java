8c404862daed31b8f3cb504a0a627767069f93a9
Vikas <vikassharmanaini@gmail.com>
2022-05-19 12:05:59 +0530

    3rd day work Completed

--- src/com/Suven/Consultants/app.java
+++ src/com/Suven/Consultants/app.java
@@ -10,7 +10,7 @@ import java.awt.event.MouseEvent;
 public class app{
     private boolean method=true;
     private JPanel label1;
-    private JTextField textField1;
+    private JTextField Balencs;
 
     public boolean isMethod() {
         return method;
@@ -20,14 +20,14 @@ public class app{
         this.method = method;
     }
 
-    private JTextField textField2;
-    private JTextField textField3;
-    private JButton xButton;
-    private JButton xButton1;
+    private JTextField intrestrate;
+    private JTextField payments;
+    private JButton cancelpayment;
+    private JButton cancelpaid;
     private JButton computeMonthlyPaymentsButton;
     private JButton newLoanAnalysisButton;
     private JTextArea textArea1;
-    private JTextField textField4;
+    private JTextField paid;
     private JButton exitButton;
     private JLabel l1;
     private JLabel l2;
@@ -35,53 +35,54 @@ public class app{
     private JLabel l4;
 
     public app() {
-        xButton1.setVisible(false);
-        textField4.setBackground(Color.YELLOW);
-        textField4.setEnabled(false);
+        cancelpaid.setVisible(false);
+        paid.setBackground(Color.YELLOW);
+        paid.setEnabled(false);
         newLoanAnalysisButton.setEnabled(false);
         textArea1.setFont(new Font("Courier New", Font.PLAIN, 14));
         textArea1.setEditable(false);
         textArea1.setPreferredSize(new Dimension(250, 150));
         textArea1.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        xButton.addMouseListener(new MouseAdapter() {
+        cancelpayment.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                xButton.setVisible(false);
-                textField3.setBackground(Color.YELLOW);
-                textField3.setEnabled(false);
-                xButton1.setVisible(true);
-                textField4.setBackground(Color.WHITE);
-                textField4.setEnabled(true);
-                setMethod(true);
+                cancelpayment.setVisible(false);
+                payments.setBackground(Color.YELLOW);
+                payments.setEnabled(false);
+                payments.setForeground(Color.BLACK);
+                cancelpaid.setVisible(true);
+                paid.setBackground(Color.WHITE);
+                paid.setEnabled(true);
+                setMethod(false);
                 System.out.println(method);
             }
         });
 
 
-        xButton1.addMouseListener(new MouseAdapter() {
+        cancelpaid.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                xButton1.setVisible(false);
-                textField4.setBackground(Color.YELLOW);
-                textField4.setEnabled(false);
-                xButton.setVisible(true);
-                textField3.setBackground(Color.WHITE);
-                textField3.setEnabled(true);
-                setMethod(false);
+                cancelpaid.setVisible(false);
+                paid.setBackground(Color.YELLOW);
+                paid.setEnabled(false);
+                paid.setForeground(Color.BLACK);
+                cancelpayment.setVisible(true);
+                payments.setBackground(Color.WHITE);
+                payments.setEnabled(true);
+                setMethod(true);
                 System.out.println(method);
             }
         });
         computeMonthlyPaymentsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (method){
-
-                   textField4.setText(String.valueOf(new LoanCalculator().paid(Double.parseDouble(textField2.getText()),Double.parseDouble(textField1.getText()),Integer.parseInt(textField3.getText()))));
+                if (isMethod()){
+                   paid.setText(String.valueOf(new LoanCalculator().paid(Double.parseDouble(intrestrate.getText()),Double.parseDouble(Balencs.getText()),Integer.parseInt(payments.getText()))));
                 }
                 else {
-                    textField3.setText(String.valueOf(new LoanCalculator().findhowmuchtimes(Double.parseDouble(textField2.getText()),Double.parseDouble(textField1.getText()),Double.parseDouble(textField4.getText()))));
+                    payments.setText(String.valueOf(new LoanCalculator().findhowmuchtimes(Double.parseDouble(intrestrate.getText()),Double.parseDouble(Balencs.getText()),Double.parseDouble(paid.getText()))));
                 }
                 newLoanAnalysisButton.setEnabled(true);
                 computeMonthlyPaymentsButton.setEnabled(false);
@@ -92,10 +93,10 @@ public class app{
             public void actionPerformed(ActionEvent e) {
                 newLoanAnalysisButton.setEnabled(false);
                 computeMonthlyPaymentsButton.setEnabled(true);
-                textField1.setText("");
-                textField2.setText("");
-                textField4.setText("");
-                textField3.setText("");
+                Balencs.setText("");
+                intrestrate.setText("");
+                paid.setText("");
+                payments.setText("");
             }
         });
     }
