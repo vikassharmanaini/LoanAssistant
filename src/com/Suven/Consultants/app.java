be751705d25827287040d8f0561d927713449800
Vikas <vikassharmanaini@gmail.com>
2022-06-03 20:22:47 +0530

    4th day work Completed

--- src/com/Suven/Consultants/app.java
+++ src/com/Suven/Consultants/app.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.text.DecimalFormat;
 
 public class app{
     private boolean method=true;
@@ -26,29 +27,32 @@ public class app{
     private JButton cancelpaid;
     private JButton computeMonthlyPaymentsButton;
     private JButton newLoanAnalysisButton;
-    private JTextArea textArea1;
+    private JTextArea Analysis;
     private JTextField paid;
     private JButton exitButton;
-    private JLabel l1;
-    private JLabel l2;
-    private JLabel l3;
-    private JLabel l4;
+    private JLabel loanBalenceLabel;
+    private JLabel intrestRateLabel;
+    private JLabel numberOfPaymentsLabel;
+    private JLabel monthlyPaymentsLabel;
+    private JLabel loanAnalysisLabel;
 
     public app() {
+        Analysis.setDragEnabled(false);
         cancelpaid.setVisible(false);
         paid.setBackground(Color.YELLOW);
         paid.setEnabled(false);
         newLoanAnalysisButton.setEnabled(false);
-        textArea1.setFont(new Font("Courier New", Font.PLAIN, 14));
-        textArea1.setEditable(false);
-        textArea1.setPreferredSize(new Dimension(250, 150));
-        textArea1.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        Analysis.setFont(new Font("Courier New", Font.PLAIN, 14));
+        Analysis.setEditable(false);
+        Analysis.setPreferredSize(new Dimension(250, 150));
+        Analysis.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         cancelpayment.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
                 cancelpayment.setVisible(false);
                 payments.setBackground(Color.YELLOW);
+                paid.setBackground(Color.getHSBColor(63,66,80));
                 payments.setEnabled(false);
                 payments.setForeground(Color.BLACK);
                 cancelpaid.setVisible(true);
@@ -65,7 +69,7 @@ public class app{
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
                 cancelpaid.setVisible(false);
-                paid.setBackground(Color.YELLOW);
+                paid.setBackground(Color.getHSBColor(63,66,80));
                 paid.setEnabled(false);
                 paid.setForeground(Color.BLACK);
                 cancelpayment.setVisible(true);
@@ -79,7 +83,23 @@ public class app{
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (isMethod()){
-                   paid.setText(String.valueOf(new LoanCalculator().paid(Double.parseDouble(intrestrate.getText()),Double.parseDouble(Balencs.getText()),Integer.parseInt(payments.getText()))));
+                    int paidz=new LoanCalculator().paid(Double.parseDouble(intrestrate.getText()),Double.parseDouble(Balencs.getText()),Integer.parseInt(payments.getText()));
+                   paid.setText(String.valueOf(paidz));
+                    Analysis.setText("Loan Balance: $"+new DecimalFormat("0.00").format(Integer.parseInt( Balencs.getText())));
+                    Analysis.append("\nInterest Rate: "+new DecimalFormat("0.00").format(Double.parseDouble(intrestrate.getText())));
+                    int month=Integer.parseInt(payments.getText());
+                    double Intrest=Double.parseDouble(intrestrate.getText())/1200;
+                    double LoanBalence=Double.parseDouble( Balencs.getText());
+                    for (int i=1;i<=Integer.parseInt(payments.getText());i++){
+
+                        LoanBalence+=LoanBalence*Intrest-paidz;
+                    }
+                    double finalpayment=LoanBalence;
+                    if (finalpayment>Double.parseDouble(Balencs.getText())){
+                        LoanBalence+=LoanBalence*Intrest-Double.parseDouble(payments.getText());
+                        finalpayment=LoanBalence;
+                        month++;
+                    }
                 }
                 else {
                     payments.setText(String.valueOf(new LoanCalculator().findhowmuchtimes(Double.parseDouble(intrestrate.getText()),Double.parseDouble(Balencs.getText()),Double.parseDouble(paid.getText()))));
